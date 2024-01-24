6d2dcbabe4b244bd15da52c8782df4619e9ed26f
sairamchavatapall <sairamch3@gmail.com>
2024-01-23 21:17:50 -0600

    controller function added

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const dotenv = require('dotenv');
 
 const initialize = async (config) => {
-
     // Load environment variables from .env file
     dotenv.config();
 
     // Override environment variables with config values
     process.env = { ...process.env, ...config };
 
-    // get sequlize database from module who using this
-
-    console.log('config.sequelize', config.sequelize)
+    // Get sequelize database from the module that's using this
+    console.log('config.sequelize', config.sequelize);
 
     global.sequelize = config.sequelize;
+}
 
+const getControllers = () => {
     const controllers = require('./Controllers');
+
     return controllers;
 }
 
-// Export everything in a single object
 module.exports = {
     initialize,
-  };
+    getControllers,
+};
